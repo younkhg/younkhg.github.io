82f3e009b21082806c3803336aa468af51234fde
Keehong Youn <younkeehong@gmail.com>
2017-08-18 04:22:51 -0700

    wip balmov

--- main.js
+++ main.js
@@ -1,22 +1,12 @@
 window.onload = function() {
-    let header = document.querySelector("header");
-    header.innerHTML = `
-        <h2><a href="index.html">Keehong Youn</a></h1>
-    `;
-
     let nav = document.querySelector("nav");
     nav.innerHTML = `
-        <a href="">link4</a>&nbsp;|
+        <a href="rotations_n3.html">Rotations, N=3</a>&nbsp;|
+        <a href="balanced_movement.html">Balanced Movement</a>&nbsp;|
         <a href="">link3</a>&nbsp;|
         <a href="">link2</a>&nbsp;|
-        <a href="rotation01.html">rotations, n=3</a>&nbsp;|
         <br>
         <a href="blog_main.html">blog</a>&nbsp;|
         <a href="about.html">about</a>&nbsp;|
-    `;
-
-    let footer = document.querySelector("footer");
-    footer.innerHTML = `
-        <p class="footers">younkeehong@gmail.com</p>
     `;
 }
