cfe56e15483233465e6b9f280c425d7fc21d935e
Keehong Youn <kyoun@apple.com>
2025-02-23 23:40:37 -0800

    cleanup

--- server/index.js
+++
@@ -1,4 +0,0 @@
-let express = require("express");
-let app = express();
-app.use(express.static(".."));
-app.listen(3000, () => console.log("listening on port 3000"));
--- server/readme.md
+++
@@ -1,5 +0,0 @@
-Runs static https server from `..`
-
-    npm install
-    node index.js
-
